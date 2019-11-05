01628b586d5ea6b9fecdb602cc0b3ecf362b5909
Xavier <xavier.binois@u-bourgogne.fr>
2019-11-05 13:32:00 +0100

    Corrections tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,13 +13,19 @@ def test_hosts_file(host):
     assert f.user == 'root'
     assert f.group == 'root'
 
+@pytest.mark.parametrize('package', [
+  'iptables'
+])
 def test_iptables_is_present(host):
-    iptables = host.package(iptables)
+    iptables = host.package(package)
 
     assert iptables.is_installed
 
+@pytest.mark.parametrize('service', [
+  'iptables'
+])
 def test_iptables_service_is_started(host):
-    service = host.service(iptables)
+    service = host.service(service)
 
     assert service.is_running
     assert service.is_enabled
