f90d7751e6b01c47f791b7b0e30041a85d691a28
Xavier <xavier.binois@u-bourgogne.fr>
2019-11-05 12:51:30 +0100

    Add tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,14 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_iptables_is_present(host):
+    iptables = host.package(iptables)
+
+    assert iptables.is_installed
+
+def test_iptables_service_is_started(host):
+    service = host.service(iptables)
+
+    assert service.is_running
+    assert service.is_enabled
